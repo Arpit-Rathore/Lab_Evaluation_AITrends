16b4e18e6ffc7e5e2b92416e2e9d9de1c48cfc57
Arpit Rathore <61356834+Arpit-Rathore@users.noreply.github.com>
2022-03-03 17:01:35 +0530

    First Commit for Lab Evaluation

---
+++ Test Code.py
@@ -0,0 +1,12 @@
+'''Python program which accept the radius of a circle from the user and compute
+the area'''
+x=float(input("Enter radius of circle: "))
+y=3.14*x*x
+print("Area of circle for given radius: ",y)
+
+
+#Output
+'''
+Enter radius of circle36.1235
+Area of circle for given radius:  4097.408772065
+'''
