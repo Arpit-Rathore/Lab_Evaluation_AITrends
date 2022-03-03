412c77cb4a687f452df061c4edcd9cb10d641ee3
Arpit Rathore <61356834+Arpit-Rathore@users.noreply.github.com>
2022-03-03 17:57:54 +0530

    Updating New file uploaded

--- New file uploaded.py
+++ New file uploaded.py
@@ -1,6 +1,6 @@
 '''Python program to convert Fahrenheit temperature into Centigrade degrees.'''
 
-f_temp=float(input("Enter temperature of the city in Fahrenheit degrees: "))
+f_temp=float(input("Kindly enter temperature of the city in Fahrenheit degrees: "))
 c_temp=(5*(f_temp-32))/9
 print("Temperature of the city in Centigrade degrees: ",c_temp)
 
